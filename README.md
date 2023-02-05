fd80befe76ef07436abbcfa62d08fc222ae49b84
Devin <marcelinus.devin@amaan.co.id>
2023-02-05 14:57:58 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# batterylevel
+
+Flutter [Lab: Platform channel Tutorial](https://docs.flutter.dev/development/platform-integration/platform-channels?tab=type-mappings-kotlin-tab)
---
+++ android/app/src/main/kotlin/com/example/batterylevel/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.example.batterylevel
+
+import io.flutter.embedding.android.FlutterActivity
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
+
+class MainActivity : FlutterActivity() {
+    private val CHANNEL = "example.flutter/platform_channel"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            CHANNEL
+        ).setMethodCallHandler { call, result ->
+            // This method is invoked on the main thread.
+            // method name same with one on Flutter app
+            when (call.method) {
+                "getBatteryLevel" -> {
+                    val batteryLevel = getBatteryLevel();
+
+                    if (batteryLevel != -1) {
+                        result.success(batteryLevel)
+                    } else {
+                        result.error("UNAVAILABLE", "Battery level not available.", null)
+                    }
+                }
+                "changeLife" -> {
+                    result.success("Hellow from Android!")
+                }
+                "getDeviceInfo" -> {
+                    val deviceInfo: HashMap<String, String> = getDeviceInfo()
+                    if (deviceInfo.isNotEmpty()) {
+                        result.success(deviceInfo)
+                    } else {
+                        result.error("UNAVAILABLE", "Device info not available.", null)
+                    }
+                }
+                else -> {
+                    result.notImplemented()
+                }
+            }
+        }
+    }
+
+    //This is the native function to get battery level
+    private fun getBatteryLevel(): Int {
+        val batteryLevel: Int
+        if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+            val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+        } else {
+            val intent = ContextWrapper(applicationContext).registerReceiver(
+                null,
+                IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+            )
+            batteryLevel =
+                intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(
+                    BatteryManager.EXTRA_SCALE,
+                    -1
+                )
+        }
+        return batteryLevel
+    }
+
+    private fun getDeviceInfo(): HashMap<String, String> {
+        val deviceInfo = HashMap<String, String>()
+        deviceInfo["version"] =
+            System.getProperty("os.version")?.toString() ?: "Version Not found" // OS version
+        deviceInfo["device"] = android.os.Build.DEVICE           // Device
+        deviceInfo["model"] = android.os.Build.MODEL            // Model
+        deviceInfo["product"] = android.os.Build.PRODUCT          // Product
+        return deviceInfo
+    }
+}
